50f6029446f918f702862dc2c9724a654007ae94
Rogger Lindao <r06xd@hotmail.es>
2024-09-10 23:24:13 -0500

    correccion variables entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_KEY: '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',  // Define aquí las variables de entorno para desarrollo
-    URL_SERV_EMAIL: 'https://api.brevo.com/v3/smtp/email/'
+    API_KEY: '',  // Define aquí las variables de entorno para desarrollo
+    URL_SERV_EMAIL: ''
   };
