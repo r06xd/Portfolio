0090258578d0d998425fd5dae4f6d7fa13340852
Rogger Lindao <r06xd@hotmail.es>
2024-09-10 23:19:18 -0500

    modificacion enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    PI_KEY: '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',  // Define aquí las variables de entorno para desarrollo
-    URL_SERV_EMAIL: 'https://api.brevo.com/v3/smtp/email/'
+    API_KEY: '',  // Define aquí las variables de entorno para desarrollo
+    URL_SERV_EMAIL: ''
   };
